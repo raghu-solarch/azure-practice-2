e5c4c4e2d0f1ad0202a125b694967024583cfcc9
raghu-solarch <raghu.solarch@gmail.com>
2025-07-12 15:44:33 +0200

    Delete multi-env-vm-creation-updated directory

--- multi-env-vm-creation-updated/modules/azure-vm/outputs.tf
+++
@@ -1,6 +0,0 @@
-output "vm_names" {
-  value = [for vm in azurerm_linux_virtual_machine.main : vm.name]
-}
-output "public_ips" {
-  value = [for pip in azurerm_public_ip.main : pip.ip_address]
-}
--- multi-env-vm-creation-updated/outputs.tf
+++
@@ -1,6 +0,0 @@
-output "vm_names" {
-  value = module.azure_vm.vm_names
-}
-output "public_ips" {
-  value = module.azure_vm.public_ips
-}
